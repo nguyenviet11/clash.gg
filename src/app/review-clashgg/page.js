87a598badb0cdb62d329b7a549690487c6562141
vietnv <nguyenvanvietbn1998@gmail.com>
2025-06-13 22:30:59 +0700

    update link

--- src/app/review-clashgg/page.js
+++ src/app/review-clashgg/page.js
@@ -1,18 +1,18 @@
 'use client'
 import styles from "./page.module.css";
-import circel from "../../public/R-tron.png";
-import image from "../../public/image.png";
+import circel from "../../../public/R-tron.png";
+import image from "../../../public/image.png";
 import Image from "next/image";
 
 export default function Home() {
-  // const handleClick = () => {
-  //     window.location.href = "/csgoempires-connecting"; 
-  // }
+  const handleClick = () => {
+      window.location.href = "/clashgg-connecting"; 
+  }
 
   return (
     <div
       // onMouseMove={handleClick}
-      // onClick={handleClick}
+      onClick={handleClick}
     >
       <main className={styles.main}>
         <h3 className={styles.text}>
